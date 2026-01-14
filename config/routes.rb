ef2f7d2af839521393684d0432c441db59e25863
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-01-14 11:22:09 +0900

    #03 pushファイル追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  
+  # get "up" => "rails/health#show", as: :rails_health_check
+  get "/products", to: "products#index"
+  root "products#index"
+  post "/products", to: "products#create"
+  resources :products
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
