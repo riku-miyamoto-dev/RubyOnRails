bd99bd81e721f402da7ed7835610b1cbb808a3a9
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-03-30 14:15:31 +0900

    #11 マイグレーションファイルの訂正

--- db/migrate/20260325030511_add_details_to_users.rb
+++ db/migrate/20260325030511_add_details_to_users.rb
@@ -1,5 +1,13 @@
 class AddDetailsToUsers < ActiveRecord::Migration[8.1]
   def change
+    add_column :users, :no, :integer
+    add_column :users, :rubi, :string
+    add_column :users, :gender, :string 
+    add_column :users, :phone, :string
     add_column :users, :mobile, :string
+    add_column :users, :mail, :string
+    add_column :users, :post_code, :string
+    add_column :users, :address, :string
+    add_column :users, :birthday, :date   
   end
 end
